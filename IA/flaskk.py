407c9879161b13699e50b5912fd325e61d54cdda
doodlefisch <romain.giraud0212@gmail.com>
2024-11-14 19:05:59 +0100

    connexion service IA et récuperation reponse

---
+++ IA/flaskk.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, jsonify
+from init_questions import generer_questions
+app = Flask(__name__)
+
+@app.route('/generer', methods=['POST'])
+def generer():
+    """
+    Endpoint pour générer des questions.
+
+    Attend un JSON avec les clés 'theme' et 'niveau_difficulte'.
+    """
+    try:
+        # Récupérer les données JSON de la requête
+        data = request.get_json()
+
+        # Extraire le thème et le niveau de difficulté
+        theme = data.get('theme')
+        niveau_difficulte = data.get('niveau_difficulte')
+
+        # Vérifier que les paramètres sont présents
+        if not theme or not niveau_difficulte:
+            return jsonify({'error': 'Les paramètres "theme" et "niveau_difficulte" sont requis.'}), 400
+
+        # Appeler la fonction logique pour générer les questions
+        questions = generer_questions(theme, niveau_difficulte)
+
+        # Retourner les résultats sous forme de JSON
+        return jsonify({'questions': questions}), 200
+
+    except Exception as e:
+        # Gérer les erreurs
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ IA/init_questions.py
@@ -0,0 +1,54 @@
+
+from google.cloud import aiplatform
+
+import os
+from google.oauth2 import service_account
+import vertexai
+from vertexai.preview.language_models import TextGenerationModel
+from vertexai.generative_models import GenerativeModel
+
+credentials = service_account.Credentials.from_service_account_file(
+    r'C:\Users\romai\Documents\ESILV A5\QuizzerAI\IA\projet1-441715-44c008773c1a.json'
+)
+PROJECT_ID = "projet1-441715"
+REGION = "us-central1"
+
+vertexai.init(
+    project = PROJECT_ID,
+    location= REGION,
+    credentials = credentials
+)
+
+
+model = GenerativeModel(model_name="gemini-1.5-flash-002")
+
+def generer_questions(theme: str, niveau_difficulte: str) -> str:
+    """
+    Génère des questions basées sur un thème et un niveau de difficulté.
+
+    Paramètres:
+    - theme (str): Le thème des questions.
+    - niveau_difficulte (str): Le niveau de difficulté des questions.
+
+    Retourne:
+    - str: Les questions générées.
+
+    
+    """
+    # Création du prompt en intégrant le thème et le niveau de difficulté
+    prompt = f"Créez cinq questions de niveau {niveau_difficulte} sur le thème suivant : {theme}. Fournissez également les réponses à ces questions."
+
+    # Génération du texte
+    response = model.generate_content(
+        prompt,
+        #temperature=0.7,
+        #max_output_tokens=512,
+        #top_p=0.8,
+        #top_k=40
+    )
+
+    # Retourne le texte généré
+    return response.text
+
+
+
