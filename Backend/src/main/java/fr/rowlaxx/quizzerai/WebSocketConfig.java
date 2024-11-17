cab4a56894e48aa99aac200307e741c51db0dd1e
Vincent <vincent.cotella@edu.devinci.fr>
2024-11-17 20:47:29 +0100

    b

--- Backend/src/main/java/fr/rowlaxx/quizzerai/WebSocketConfig.java
+++ Backend/src/main/java/fr/rowlaxx/quizzerai/WebSocketConfig.java
@@ -2,6 +2,9 @@ package fr.rowlaxx.quizzerai;
 
 import fr.rowlaxx.quizzerai.game.GameNotifier;
 import lombok.AllArgsConstructor;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
@@ -18,5 +21,15 @@ public class WebSocketConfig implements WebSocketConfigurer {
         registry.addHandler(notifier, "/game/live");
     }
 
+    @Bean
+    public WebServerFactoryCustomizer<TomcatServletWebServerFactory> customizer() {
+        return factory -> factory.addAdditionalTomcatConnectors(createHttpConnector());
+    }
 
+    private org.apache.catalina.connector.Connector createHttpConnector() {
+        org.apache.catalina.connector.Connector connector =
+                new org.apache.catalina.connector.Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+        connector.setPort(8544); // HTTP port for WebSocket
+        return connector;
+    }
 }
